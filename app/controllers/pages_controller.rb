73600e09777d92498992547f604927851f3a72e9
maxpleaner <maxpleaner@gmail.com>
2015-05-26 23:28:31 +0000

    add resume endpoint

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,10 @@ class PagesController < ApplicationController
 
   def animation
   end
+  
+  def resume
+    pdf_filename = File.join(Rails.root, "public/MaxPleanersResume.pdf")
+    send_file(pdf_filename, :filename => "your_document.pdf", :disposition => "inline", :type => "application/pdf")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'animation', to: "pages#animation"
+  get 'resume', to: "pages#resume"
 end
