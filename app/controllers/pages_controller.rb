59a6d60085e05022aa5875e9c648737490a0d12d
maxpleaner <maxpleaner@gmail.com>
2016-01-26 13:08:47 -0800

    OK

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,16 @@
 class PagesController < ApplicationController
   def index
     @urls = PagesHelper::Links
+    # render text: File.read(Rails.root.join("public/weebly.html"))
   end
 
   def animation
   end
   
   def resume
-    pdf_filename = File.join(Rails.root, "public/MaxPleanersResume.pdf")
-    send_file(pdf_filename, :filename => "your_document.pdf", :disposition => "inline", :type => "application/pdf")
+    @resume = File.read(Rails.root.join("public", "resume_export.html"))
+    # pdf_filename = File.join(Rails.root, "public/MaxPleanersResume.pdf")
+    # send_file(pdf_filename, :filename => "your_document.pdf", :disposition => "inline", :type => "application/pdf")
   end
   
   def portfolio
